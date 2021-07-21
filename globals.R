5f99fee9abb797a0e2e5a6b17517d454f692664b
Mark Sellors <github@5vcc.com>
2021-07-21 09:34:22 +0100

    Improves and simplifies stringsAsFactors handling

--- globals.R
+++ globals.R
@@ -1,7 +1,7 @@
 # Global functions and such
 
 ## Make our big data frame
-make_df <- function(nrow, ncol) {
+make_df <- function(nrow, ncol, stringsAsFactors) {
   cbind(
     as.data.frame(matrix(
       runif(nrow * ncol), nrow = nrow, ncol = ncol
@@ -13,24 +13,8 @@ make_df <- function(nrow, ncol) {
     rep(
       'THIS IS A DIFFERENT VERY LONG TEXT STRING TO TEST A THEORY ABOUT THE STRING CACHE THIS IS A VERY LONG TEXT STRING TO TEST A THEORY ABOUT THE STRING CACHE',
       nrow
-    )
+    ), stringsAsFactors = stringsAsFactors
   )
-}
-
-## Make our big data frame
-make_df_factors <- function(nrow, ncol) {
-  cbind(
-    as.data.frame(matrix(
-      runif(nrow * ncol), nrow = nrow, ncol = ncol
-    )),
-    rep(
-      'THIS IS A VERY LONG TEXT STRING TO TEST A THEORY ABOUT THE STRING CACHE THIS IS A VERY LONG TEXT STRING TO TEST A THEORY ABOUT THE STRING CACHE',
-      nrow
-    ),
-    rep(
-      'THIS IS A DIFFERENT VERY LONG TEXT STRING TO TEST A THEORY ABOUT THE STRING CACHE THIS IS A VERY LONG TEXT STRING TO TEST A THEORY ABOUT THE STRING CACHE',
-      nrow
-    ), stringsAsFactors = TRUE)
 }
 
 
@@ -41,11 +25,9 @@ create_data <- function(large = FALSE, stringsAsFactors = FALSE){
     rows_needed <- 16380102
   }
 
-  if(isFALSE(stringsAsFactors)){
-    df <- make_df(nrow = rows_needed, ncol = 3)
-  } else {
-    df <- make_df_factors(nrow = rows_needed, ncol = 3)
-  }
+
+  df <- make_df(nrow = rows_needed, ncol = 3, stringsAsFactors = stringsAsFactors)
+
 
   for (i in 1:20) {
     assign(paste0("df", i), df, envir = .GlobalEnv)
